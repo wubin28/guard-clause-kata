eb80953f75f60396729a0552dad78ad22ebdacc1
benwu <wubinben@icloud.com>
2019-07-10 14:15:56 +0800

    refactor

--- src/main/java/kata/guard/Order.java
+++ src/main/java/kata/guard/Order.java
@@ -2,18 +2,26 @@ package kata.guard;
 
 public class Order {
     public void submit(Takeout takeout) {
-        if (takeout.address() == null) {
-            throw new IllegalArgumentException("Address should not be null.");
-        }
-        if ("".equals(takeout.address())) {
-            throw new IllegalArgumentException("Address should not be empty.");
-        }
-        if (takeout.receiverName() == null) {
-            throw new IllegalArgumentException("Receiver name should not be null.");
-        }
-        if ("".equals(takeout.receiverName())) {
-            throw new IllegalArgumentException("Receiver name should not be empty.");
-        }
+        guard(takeout);
         // submit order
+    }
+
+    private void guard(Takeout takeout) {
+        shouldNotBeNull(takeout.address(), "Address should not be null.");
+        shouldNotBeEmpty(takeout.address(), "Address should not be empty.");
+        shouldNotBeNull(takeout.receiverName(), "Receiver name should not be null.");
+        shouldNotBeEmpty(takeout.receiverName(), "Receiver name should not be empty.");
+    }
+
+    private void shouldNotBeEmpty(String address, String errorMessage) {
+        if ("".equals(address)) {
+            throw new IllegalArgumentException(errorMessage);
+        }
+    }
+
+    private void shouldNotBeNull(String address, String errorMessage) {
+        if (address == null) {
+            throw new IllegalArgumentException(errorMessage);
+        }
     }
 }
